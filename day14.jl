181fa3b4a31e25fc80819a25908b3ad2b45ced0c
Grégoire de Massé <demasse.gregoire@yahoo.fr>
2020-12-19 00:01:33 +0100

    Day 14 part 2

--- day14.jl
+++ day14.jl
@@ -5,7 +5,7 @@ bitarray_to_int(ba) = sum(2 .^ [35:-1:0;] .* ba)
 @assert bitarray_to_int(collect("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X") .== '0') == 2
 
 function get_init_prog(data)
-    get_ins_mask(mask) = (:mask, (set=bitarray_to_int(mask .== '1'), reset=bitarray_to_int(mask .!= '0'), floating=mask .!= 'X'))
+    get_ins_mask(mask) = (:mask, (set=bitarray_to_int(mask .== '1'), reset=bitarray_to_int(mask .!= '0'), floating=mask .== 'X'))
     get_ins_write(addr, value) = (:write, (addr=parse(UInt64, addr), value=parse(UInt64, value)))
     data = split.(data, " = ")
     [
@@ -37,30 +37,48 @@ function solve(init_prog)
     sum(values(mem))
 end
 
-function get_addrs()
+@assert solve(test_prog) == 165
 
+function get_addrs(floating)
+    nb = sum(floating)
+    ba = BitArray(zeros(nb))
+    po2 = 2 .^ [35:-1:0;][findall(floating)]
+    r = []
+    for i in 1:(2^nb)
+        ba.chunks[1] = i
+        push!(r, sum(ba .* po2))
+    end
+    r
 end
+
+@assert Set(get_addrs(BitArray([zeros(32)..., 1, 0, 1, 1]))) == Set([0, 1, 2, 3, 8, 9, 10, 11])
 
 function solve2(init_prog)
     mem = Dict()
-    set, reset = (0, 0)
+    set, reset, floating = (0, 0, 0)
     for (ins, args) in init_prog
         if ins == :mask
             set, reset, floating = args
         elseif ins == :write
-            for address in get_addrs(floating)
-                address = args.addr | set
-                mem[address] = value
+            base_addr = args.addr & ~sum(floating .* 2 .^ [35:-1:0;]) | set
+            for floating_address in get_addrs(floating)
+                address = base_addr | floating_address
+                mem[address] = args.value
             end
         end
     end
-    @show mem
     sum(values(mem))
 end
 
-@assert solve(test_prog) == 165
+test_input = readlines(IOBuffer("""mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1"""))
+
+@assert solve2(get_init_prog(test_input)) == 208
 
 open("inputs/14.txt") do file
     init_prog = get_init_prog(readlines(file))
     println("Part 1: $(solve(init_prog))")
+    println("Part 2: $(solve2(init_prog))")
 end
